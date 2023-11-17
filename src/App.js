ea54245cf70f8c419e7a64df0fb4b46be3881a6a
Denise <deniselewis1120@gmail.com>
2023-11-16 17:33:38 -0800

    Initial draft of input form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,49 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <header>
+        <div>
+          <h1>Whisk & Wonder</h1>
+          <h2>Baking Ideas</h2>
+          <aside className="controls">
+            <button onClick={() => {}} className="add-new">
+              + New Idea
+            </button>
+            <input
+              type="text" 
+              placeholder="Type here to search..." 
+              className="search" 
+              value={""}
+              onChange={() => {}}
+            />
+          </aside>
+        </div>
+      </header>
+      <main>
+        <section className="all-ideas">
+          <ul className="ideas-list">
+            <li className="card">
+              <input
+                type="text"
+                placeholder="Name"
+                className="idea-name"
+                value={""}
+                onChange={() => {}}
+              />
+              <textarea
+                placeholder="Description..."
+                className="idea-description"
+                value={""}
+                onChange={() => {}}
+              />
+              <span className="idea-delete" onClick={() => {}}>
+                X
+              </span>
+            </li>
+          </ul>
+        </section>
+      </main>
+      <footer>&copy; 2023 Whisk & Wonder</footer>
     </div>
   );
 }
