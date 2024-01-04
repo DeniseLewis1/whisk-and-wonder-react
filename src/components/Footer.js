8fb958fb59caea8963c78c6d776efe64603ef014
Denise <deniselewis1120@gmail.com>
2024-01-04 13:36:46 -0800

    Update year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <footer className="wrapper">
-            <div>&copy; 2023 Whisk & Wonder</div>
+            <div>&copy; {new Date().getFullYear()} Whisk & Wonder</div>
         </footer>
     );
 };
